55f089531cf9c3a94ef98526db4615a404a7ade8
karthikjn01 <karthikjn01@gmail.com>
2024-08-27 16:23:53 +0100

    added robots.txt config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [mdx(), sitemap(), tailwind(), react(), robotsTxt()],
   image: {
     domains: ['https://pub-d7a5dd1decd34960bb8440c6b14a7f34.r2.dev/']
   }
